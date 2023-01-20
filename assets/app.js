c1106f4b5a0f05c930f55cc3fa19b7284e477f69
Tsiory <fordevproject@gmail.com>
2023-01-20 18:15:17 +0300

    Rename botanique to bonique in app

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-import './botanique/assets/scss/style.scss';
+import './bonique/assets/scss/style.scss';
 
 // start the Stimulus application
 import './bootstrap';
