574447bf387d5def203a0ad78abc39c745bb3616
Tsiory <fordevproject@gmail.com>
2023-01-20 16:32:44 +0300

    Templates (assets)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,40 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\MailService;
 use App\Form\ContactUsType;
 use App\Model\Contact;
 class HomeController extends AbstractController
-{
-    private $mailService;
-    public function __construct(MailService $mailService){
-        $this->mailService = $mailService;
-    }
+{   
     #[Route('/', name: 'app_home')]
     public function index(Request $request): Response
     {
-        $contact = new Contact();
-        $contactUsForm = $this->createForm(ContactUsType::class, $contact);
-        
-        $contactUsForm->handleRequest($request);
-        if ($contactUsForm->isSubmitted() && $contactUsForm->isValid()) {
-            $contact = $contactUsForm->getData();
-            $mail = [
-                'to' => $this->getParameter('contact_recipient_mail'),
-                'from' => $contact->getEmail(),
-                'subject' => "Message de ".$contact->getLastname(),
-                'body' => $this->mailService->renderTwig('emails/contact.html.twig', ['contact' => $contact])
-            ];
-            $this->mailService->sendMail($mail);
-            unset($contact);
-            unset($contactUsForm);
-            $contact = new Contact();
-            $contactUsForm = $this->createForm(ContactUsType::class, $contact);
-        }
-        return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
-            'contactUsForm' => $contactUsForm->createView()
-        ]);
+        return $this->render('home/home.html.twig', []);
     }
 
     #[Route('/legal_notice', name: 'app_legal_notice')]
