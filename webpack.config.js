0aadae7017dc1e23d234bc8d31303401ab1f4a88
Tsiory <fordevproject@gmail.com>
2023-01-20 17:18:53 +0300

    configure assets with encore webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,39 @@ Encore
     .addEntry('loader_js', './assets/js/loader.js')
     
     // Botanique: JS
+    .addEntry('botanique_modernizr_js', './assets/botanique/assets/js/vendor/modernizr-3.7.1.min.js')
+    .addEntry('botanique_jquery_js', './assets/botanique/assets/js/vendor/jquery-3.4.1.min.js')
+    .addEntry('botanique_popper_js', './assets/botanique/assets/js/popper.js')
+    .addEntry('botanique_bootstrap_js', './assets/botanique/assets/js/bootstrap.min.js')
+    .addEntry('botanique_carousel_js', './assets/botanique/assets/js/owl.carousel.min.js')
+    .addEntry('botanique_slick_js', './assets/botanique/assets/js/slick.min.js')
+    .addEntry('botanique_jquery_manific_popup_js', './assets/botanique/assets/js/jquery.magnific-popup.min.js')
+    .addEntry('botanique_jquery_counterup_js', './assets/botanique/assets/js/jquery.counterup.min.js')
+    .addEntry('botanique_jquery_countdown_js', './assets/botanique/assets/js/jquery.countdown.js')
+    .addEntry('botanique_jqurery_ui_js', './assets/botanique/assets/js/jquery.ui.js')
+    .addEntry('botanique_jquery_elevatezoom_js', './assets/botanique/assets/js/jquery.elevatezoom.js')
+    .addEntry('botanique_isotope_pkgd_js', './assets/botanique/assets/js/isotope.pkgd.min.js')
+    .addEntry('botanique_slinky_menu_js', './assets/botanique/assets/js/slinky.menu.js')
+    .addEntry('botanique_plugins_js', './assets/botanique/assets/js/plugins.js')
+    
     .addEntry('botanique_main_js', './assets/botanique/assets/js/main.js')
 
-    
+
     
     // Botanique: CSS
+    .addStyleEntry('botanique_bootstrap_css', './assets/botanique/assets/css/bootstrap.min.css')
+    .addStyleEntry('botanique_owl_carousel_css', './assets/botanique/assets/css/owl.carousel.min.css')
+    .addStyleEntry('botanique_slick_css', './assets/botanique/assets/css/slick.css')
+    .addStyleEntry('botanique_magnific_popup_css', './assets/botanique/assets/css/magnific-popup.css')
+    .addStyleEntry('botanique_font_awesome_css', './assets/botanique/assets/css/font.awesome.css')
+    .addStyleEntry('botanique_ionicons_css', './assets/botanique/assets/css/ionicons.min.css')
+    .addStyleEntry('botanique_simple_line_icons_css', './assets/botanique/assets/css/simple-line-icons.css')
+    .addStyleEntry('botanique_animate_css', './assets/botanique/assets/css/animate.css')
+    .addStyleEntry('botanique_jquery_ui_css', './assets/botanique/assets/css/jquery-ui.min.css')
+    .addStyleEntry('botanique_slinky_menu_css', './assets/botanique/assets/css/slinky.menu.css')
+    .addStyleEntry('botanique_plugins_css', './assets/botanique/assets/css/plugins.css')
+
+
     .addStyleEntry('botanique_style_css', './assets/botanique/assets/css/style.css')
     .addStyleEntry('botanique_custom_css', './assets/botanique/assets/css/custom.css')
     
@@ -84,7 +112,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
